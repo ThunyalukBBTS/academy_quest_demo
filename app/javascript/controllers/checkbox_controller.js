991d00315e0a84331f47d09496f3e008232149b2
thunyalukBBTS <thunyaluk.sa@kkumail.com>
2025-08-16 22:33:19 +0700

    style: checkbox and quest name text sizing

--- app/javascript/controllers/checkbox_controller.js
+++ app/javascript/controllers/checkbox_controller.js
@@ -2,9 +2,6 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="checkbox"
 export default class extends Controller {
-  connect() {
-  }
-
   submit(event) {
     this.element.closest("form").requestSubmit();
   }
